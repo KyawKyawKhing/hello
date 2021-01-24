bd2759f8b8eb3245d9edd2583896416c65d0f589
Kyaw Kyaw Khing <kyawkyawkhing@aceplussolutions.com>
2021-01-24 21:35:48 +0630

    add toast

--- lib/src/main/java/net/mbonnin/jitpackandroidsample/Hello.kt
+++ lib/src/main/java/net/mbonnin/jitpackandroidsample/Hello.kt
@@ -1,5 +1,11 @@
 package net.mbonnin.jitpackandroidsample
 
+import android.content.Context
+import android.widget.Toast
+
 fun hello() {
     println("Hello Jitpack!")
+}
+fun showToast(context:Context,message:String){
+    Toast.makeText(context,message,Toast.LENGTH_SHORT).show()
 }
